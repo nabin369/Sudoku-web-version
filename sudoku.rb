e1f0612be8e5cab49f1957185ba2dc12312d9dd4
nabin369 <nabinrai369@gmail.com>
2014-02-05 23:05:00 +0000

    updated grid with unsolved puzzle

--- sudoku.rb
+++ sudoku.rb
@@ -2,18 +2,26 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+# enable :sessions
+
 def random_sudoku
 	# we're using 9 numbers, 1 to 9, and 72 zeros as an input
 	# it's ovious ther may be no clashes as all umbers are unique
-	seed = (1..9).to_a.shuffle + Array.new(81-9,0) 
+	seed = (1..9).to_a.shuffle + Array.new(81-9,0)  
 	sudoku = Sudoku.new(seed.join)
 	# then we solve this (really hard!) sudoku
-	sudoku.solve!
 	# and give the output to the vies as an array of chars
 	sudoku.to_s.chars
 end
 
+# this method removes some digits from the solution to create a puzzle
+# def puzzle(sudoku)
+# 	sudoku.solve!
+# end
+
 get '/' do # default route for our website
+	# sudoku = random_sudoku
+	# sessions[:solution] = sudoku
 	@current_solution = random_sudoku
 	erb :index
 end
